9cca4ebf641fdf6a2c702f6438ab0ccbe995bae8
Asher Huang <whuang@onlyou.com>
2024-03-07 14:30:59 +0800

    添加创建商品页，查看商品详情页

--- src/main/java/com/miaoshaproject/error/Test.java
+++ src/main/java/com/miaoshaproject/error/Test.java
@@ -1,5 +1,7 @@
 package com.miaoshaproject.error;
 
+import java.math.BigDecimal;
+
 /**
  * @Author:asher
  * @Date:7/14/23 11:56
@@ -10,5 +12,17 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(EmBusinessErr.class);
         System.out.println(EmBusinessErr.class.getSuperclass());
+        double d1=1000.0,d2=0.001;
+        double d3=d1+d2;
+        System.out.println("d3=" + d3);
+        double a = 0.02;
+        double b = 0.03;
+        double c = b - a;
+        System.out.println(c);
+
+        BigDecimal _a = new BigDecimal("0.02");
+        BigDecimal _b = new BigDecimal("0.03");
+        BigDecimal _c = _b.subtract(_a);
+        System.out.println(_c);
     }
 }
